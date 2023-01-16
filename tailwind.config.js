64ea9e966e7d085d6577672776ae5df31d8ff6e0
Girotto <l.girotto@campus.fct.unl.pt>
2023-01-16 08:34:59 +0000

    base done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./*html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      "custom-mod-violet": "#7541c8",
+      "custom-very-dark-grayish-blue": "#48556a",
+      "custom-very-dark-blackish-blue": "#19212e",
+      "custom-light-gray": "#cfcfcf",
+      "custom-light-grayish-blue": "#ecf2f8",
+      }
+    },
+    fontFamily: {
+      "Barlow Semi Condensed": ["Barlow Semi Condensed", "sans-serif"]
+    }
+    
   },
   plugins: [],
 }
